1cc8c506be8ed645508060d4f66baa637faf585c
lolikeketon <hahawqwqofficial@gmail.com>
2025-11-05 10:38:31 +0500

    Add README.md and requirements.txt

--- main.py
+++ main.py
@@ -8,5 +8,7 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    # Программируем здесь
+    active_passcards = Passcard.objects.filter(is_active=True)
+
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Активных пропусков:', active_passcards.count())
--- settings.py
+++ settings.py
@@ -1,17 +1,29 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+host = os.getenv('HOST')
+port = os.getenv('PORT')
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
+key = os.getenv('SECRET_KEY')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': host,
+        'PORT': port,
+        'NAME': 'checkpoint',
+        'USER': user,
+        'PASSWORD': password,
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = key
 
 TIME_ZONE = 'Europe/Moscow'
 
